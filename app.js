9c212314548a2cd7e63b10a2408f2ded6805ce3c
mathew tan <mathew12tan@gmail.com>
2021-09-06 09:46:21 +0800

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+let timerContainer = document.querySelector(".timer-container");
+const timeArray = ["days", "hours", "minutes", "seconds"];
+
+function createTimer(timeArray) {
+  let displayTimer = timeArray.map(function (item) {
+    return `<div class="timer">
+              <div class="card">
+                <div class="card-top"><span class="next"></span></div>
+                <div class="card-bottom"><span class="prev"></span></div>
+              </div>
+              <div class="card-flip">
+                <div class="card-flip-front"><span class="prev"></span></div>
+                <div class="card-flip-back"><span class="next"></span></div>
+              </div>
+              <p>${item}</p>
+          </div>`
+  });
+  displayTimer = displayTimer.join("");
+  timerContainer.innerHTML = displayTimer;
+}
+
+let timers = [];
+window.addEventListener("DOMContentLoaded", function () {
+  createTimer(timeArray);
+  timers = document.querySelectorAll(".timer");
+})
+
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 let currentMonth = currentDate.getMonth();
@@ -6,10 +33,9 @@ let currentHour = currentDate.getHours();
 let currentMinute = currentDate.getMinutes();
 let currentSecond = currentDate.getSeconds();
 
-const futureDate = new Date(currentYear, currentMonth, currentDay + 8, currentHour + 23,
-  currentMinute + 0, currentSecond + 10);
+const futureDate = new Date(currentYear, currentMonth, currentDay + 1, currentHour + 0,
+  currentMinute + 0, currentSecond + 5);
 
-const timers = document.querySelectorAll(".timer");
 const launchBtn = document.querySelector(".launch-btn");
 
 const flipSound = new Audio("./sound/flip.mp3");
@@ -23,7 +49,6 @@ function format(item) {
 }
 
 function getRemainingTime() {
-
   const futureTime = futureDate.getTime();
   const today = new Date().getTime();
   const remainingTime = futureTime - today;
@@ -66,7 +91,7 @@ function getRemainingTime() {
     setTimeout(function () {
       document.querySelector(".heading").textContent = "Yay, we've launched!";
       launchBtn.style.display = "block";
-      launchBtn.addEventListener("click", function(){
+      launchBtn.addEventListener("click", function () {
         window.location.href = "https://github.com/mathew12tan?tab=repositories";
       })
     }, 1500)
